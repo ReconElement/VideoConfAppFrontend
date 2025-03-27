207ee9bb50c2a96752d2e90a4fff27cf22b5996d
ReconElement <132275628+ReconElement@users.noreply.github.com>
2025-03-27 20:52:14 +0530

    Delete hello.ts

--- api/hello.ts
+++
@@ -1,6 +0,0 @@
-import {Request} from "@vercel/functions";
-export async function GET(req: Request){
-    const url = new URL(req?.url);
-    const name = url.searchParams.get('name') || 'World';
-    return Response.json({message: `Hello ${name}`});
-}
